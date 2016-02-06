89d5562bae50812ee396a3e1f8cec6baa65bfe95
Vasily Klenov <klenov@gmail.com>
2016-02-06 03:52:29 +0300

    added DebugMessage.h/.cpp

---
+++ debugMessage.h
@@ -0,0 +1,39 @@
+#ifndef DEBUG_MESSAGES
+#define DEBUG_MESSAGES
+
+#include <inttypes.h>
+#include <stdarg.h>
+
+#include "Arduino.h"
+
+#define CR "\n"
+
+class DebugMessage {
+public:
+    enum Level { NOOUTPUT, ERROR, INFO, DEBUG, VERBOSE };
+private:
+    Level   _level;
+    Level   _level_current;
+    Print*  _printer;
+public:
+  /*! * default Constructor */
+    DebugMessage()
+      : _level(Level::NOOUTPUT),
+        _level_current(Level::NOOUTPUT),
+        _printer(nullptr) {}
+
+    void setPrinter(Print &printer);
+    void setLevel(Level level);
+
+    DebugMessage& level(Level level);
+    DebugMessage& print(const char* msg);
+    DebugMessage& println(const char* msg);
+    DebugMessage& printf(const char* msg, ...);
+
+private:
+    void va_print(const char *format, va_list args);
+};
+
+extern DebugMessage debugMessage;
+typedef DebugMessage DM;
+#endif
