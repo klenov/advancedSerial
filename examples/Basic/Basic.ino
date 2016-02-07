70b4c34faf96987a7a0337b471e0e23f08ad27b7
Vasily Klenov <klenov@gmail.com>
2016-02-07 04:00:08 +0300

    updated example

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -2,21 +2,31 @@
 
 #include <debugMessage.h>
 
-int    counter = 0;
+int counter = 0;
 
 void setup() {
   Serial.begin(9600);
 
   dMessage.setPrinter(Serial);
-  dMessage.setLevel(Level::INFO); // set verbosity level, could be OFF, ERROR, INFO, DEBUG, VERBOSE
+  dMessage.setLevel(Level::DEBUG); // set verbosity level, could be OFF, ERROR, INFO, DEBUG, VERBOSE
 }
 
 void loop() {
-  /* The following message will be printed at Level::INFO */
-  dMessage.l(Level::INFO).printf("counter = %d" CR, counter);
-  counter += 1;
+  /* This message will be printed at Level::INFO */
+  dMessage.level(Level::INFO).print("counter = ").println(counter);
 
-  /* The following message will be printed only after setLevel(Level::VERBOSE) */
-  dMessage.l(Level::VERBOSE).printf("End of loop()" CR);
-  delay(200);
+  /* The same with method aliases */
+  dMessage.l(Level::INFO).p("counter = ").pln(counter);
+
+  /* Of course you can still use good old Serial.print() */
+  Serial.print("counter = ");
+  Serial.println(counter);
+
+  dMessage.pln("This message is at Level::INFO, since printing level hasn't been changed since last call to dMessage.level()");
+  dMessage.l(Level::DEBUG).pln("This message is at Level::DEBUG");
+
+  dMessage.l(Level::VERBOSE).p("This message won't be printed until level is not set to Level::VERBOSE");
+
+  counter += 1;
+  delay(3000);
 }
